f8a7310bf99131ea11897c57acb58914dbde89ce
DCamaron1 <league@Mac-mini-3.local>
2017-10-21 12:17:08 -0700

    10-21-17

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,9 +1,12 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
@@ -11,23 +14,27 @@ public class WhackAMole implements ActionListener {
 	JFrame frame;
 	JPanel panel;
 	Random generator = new Random();
-	
+	int count;
+	Date gameStart = new Date();
+	long start = gameStart.getTime();
 	
 	public void createUI() {
-		int numGen = generator.nextInt(20 + 1);
+		int numGen = generator.nextInt(19 + 1);
 		frame = new JFrame();
 		panel = new JPanel();
 		frame.add(panel);
 		frame.setVisible(true);
-		frame.setSize(500, 400);
+		panel.setBackground(Color.blue);
+		frame.setSize(400, 300);
 		for (int i = 1; i <= 20; i++) {
-			JButton button = new JButton();		
+			JButton button = new JButton();
+			frame.setBackground(Color.white);
 			button.addActionListener(this);
 			panel.add(button);
 			if (i == numGen) {
 				button.setText("mole");
 			}
-
+		//frame.setBackground(Color.RED);
 		}
 
 	}
@@ -41,9 +48,25 @@ public class WhackAMole implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		JButton buttonClicked = (JButton) e.getSource();
+		
 		if (buttonClicked.getText().equals("mole")) {
 			System.out.println("hi");
+			count++;
+			frame.dispose();
+			createUI();
 		}
-		frame.dispose();
+		else{
+			frame.dispose();
+			createUI();
+			
+		}
+		if (count == 10) {
+			Date gameEnd= new Date(); 
+			long end = gameEnd.getTime();
+			double time = (end - start)/1000.0; 
+			frame.dispose();
+			JOptionPane.showMessageDialog(null, "You took " + time + " seconds to whack 10 moles.");
+		}
+
 	}
 }
